af52d16c88d9e51dc92378060485f97c3ed3b7c0
Pratama89 <pratama.adiciptawan@gmail.com>
2023-07-17 16:15:28 +0700

    membuat User Password

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -4,5 +4,18 @@ const User = require("../models/useModel");
 
 const createUser = async (req, res) => {
     const email = req.body.email;
-    const find = req.body.find;
+    const findUser = await User.findOne(email);
+    if (!findUser) {
+        // Membuat User Baru
+        const newUser = User.create(req.body);
+        res.json(newUser);
+    } else {
+        // User yang Sudah ada
+        res.json({
+            msg: "User Sudah Ada",
+            success: false,
+        });
+    }
 };
+
+module.exports = { createUser };
