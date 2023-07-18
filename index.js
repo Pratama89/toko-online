5662a3b6915d17dc468bb7232080daaf163dbdfd
Pratama89 <pratama.adiciptawan@gmail.com>
2023-07-18 16:12:55 +0700

    tambah tes data

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const dbConnect = require('./config/dbConnect');
+const bodyParser = require("body-parser");
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
 const app = express()
-const dotenv = require('dotenv').config()
+const dotenv = require("dotenv").config()
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 dbConnect();
 
-app.use('/', (req, res) => {
-    res.send("Halo Pharmadent");
-});
 
 
-app.use('app/user', authRouter);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use("/api/user", authRouter);
 app.listen(PORT, () => {
     console.log(`Server Berjalan pada PORT ${PORT}`);
 });
--- models/useModel.js
+++ models/useModel.js
@@ -1,30 +1,30 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    firtname:{
+    firstname: {
         type:String,
         required:true,
     },
-    lastname:{
+    lastname: {
         type:String,
         required:true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
+    email: {
         type:String,
         required:true,
         unique:true,
     },
-    password:{
+    mobile: {
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password: {
         type:String,
         required:true,
     },
 });
 
 //Export the model
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
